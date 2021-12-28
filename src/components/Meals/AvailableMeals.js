a7e8ea7150c0a94bed29327f0c8afb86a009ce04
Daniela Brailovsky <dbrailo@gmail.com>
2021-12-28 21:46:50 +0200

    first commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,46 @@
+import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Hummus with mushrooms',
+      description: 'The best Hummus with fresh mushrooms',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Hummus with eggs',
+      description: 'The best Hummus with 3 boiled eggs',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+
+    return (
+        <section className={classes.meals}>
+          <Card> 
+            <ul>
+                {mealsList}
+            </ul>
+          </Card>
+        </section>
+    );
+};
+
+export default AvailableMeals;
